b924d4ada4912b601d3c1bca3b27c4e16aee27f9
Mitul Golakiya <mtl.golakiya@gmail.com>
2021-12-28 10:15:35 +0530

    style: Apply fixes from StyleCI (#2)
    Co-authored-by: Mitul Golakiya <mitulgolakiya@users.noreply.github.com>

--- src/Utils/LaravelEnvatoUtils.php
+++ src/Utils/LaravelEnvatoUtils.php
@@ -11,7 +11,7 @@ use InfyOmLabs\LaravelEnvato\Exceptions\EnvatoRateLimitException;
 class LaravelEnvatoUtils
 {
     /**
-     * @param  Exception  $e
+     * @param Exception $e
      *
      * @throws EnvatoException
      * @throws EnvatoRateLimitException
