956c02c459a2daca831a99211de98d3a9853f19a
Mitul Golakiya <mtl.golakiya@gmail.com>
2022-05-31 13:18:35 +0530

    feat: email and account functions added

--- src/Managers/UserManager.php
+++ src/Managers/UserManager.php
@@ -13,4 +13,24 @@ class UserManager extends BaseManager
     {
         return $this->envatoClient()->performGet('v1/market/private/user/username.json');
     }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return \InfyOmLabs\LaravelEnvato\Client\EnvatoResponse
+     */
+    public function getEmail()
+    {
+        return $this->envatoClient()->performGet('v1/market/private/user/email.json');
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return \InfyOmLabs\LaravelEnvato\Client\EnvatoResponse
+     */
+    public function accountDetails()
+    {
+        return $this->envatoClient()->performGet('v1/market/private/user/account.json');
+    }
 }
